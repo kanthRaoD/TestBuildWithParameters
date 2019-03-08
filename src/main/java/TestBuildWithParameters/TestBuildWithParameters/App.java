9dd1a3f88cbea65e2ee0083970d7c067986874b0
Sasikanth <shashijagtap9@gmail.com>
2019-03-08 10:48:50 +0530

    first commit

---
+++ src/main/java/TestBuildWithParameters/TestBuildWithParameters/App.java
@@ -0,0 +1,13 @@
+package TestBuildWithParameters.TestBuildWithParameters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
